838591a5c3f9f3a1a641f873342ea1a1492b76cd
MagturoJoel <JoelMagturoJr@gmail.com>
2022-03-30 19:01:16 +0800

    My Educational Background added

--- git-python-magturo.py
+++ git-python-magturo.py
@@ -3,3 +3,9 @@ print("Joel M. Magturo Jr.")
 print("Bunganay Boac Marinduque")
 
 print("October 30, 1999")
+
+print("Marinduque State College MSC")
+
+print("Information Technology")
+
+print("BSI/T 2C common")
